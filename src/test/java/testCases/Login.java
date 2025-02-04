536a6e1d1b2eb6c834dd226863a925ab5f5a2c67
sanju <sanju.scaria@gmail.com>
2025-02-04 12:22:18 +0530

    first commint

---
+++ src/test/java/testCases/Login.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	public void loginOne()
+	{
+		System.out.println("login one");
+	}
+	
+	public void loginTwo()
+	{
+		System.out.println("Login two");
+	}
+	
+
+}
---
+++ src/test/java/testCases/ShoppingCart.java
@@ -0,0 +1,27 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class ShoppingCart {
+
+	@Test
+	public void searchForProducts()
+	{
+		System.out.println("Search for products");
+		
+	}
+	@Test
+	public void addToCart()
+	{
+		
+		System.out.println("Add item to cart..");
+	}
+	@Test
+	public void clickSubmit()
+	{
+		
+		System.out.println("Click submit button");
+	}
+	
+	
+}
