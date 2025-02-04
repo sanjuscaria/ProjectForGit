60a04b752494d8b68fa10c41df6ee51dcdeaec84
sanju <sanju.scaria@gmail.com>
2025-02-04 12:57:08 +0530

    my second commit

---
+++ src/test/java/testCases/MakePayment.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class MakePayment {
+	
+	
+	@Test
+	public void payUsingCreditcard() {
+		System.out.println("make the payment using credit card.");
+	}
+	
+	
+
+}
